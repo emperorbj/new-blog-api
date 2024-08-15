5c39c97e020a42aff2c6f34b7a1c01328b0415eb
emperorbj <cwaku96@gmail.com>
2024-08-15 17:50:24 +0100

    "Updated users controller and route: added login functionality, reformatted code, and removed commented out code"

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,58 +1,80 @@
 const User = require('../models/users.model');
 const bcrypt = require('bcryptjs');
 
+
+
+
+
 const getAllUsers = async (req, res, next) => {
-    try{
+    try {
         const users = await User.find({});
-        if(!users){
-            return res.status(404).json({message: 'No users found'});
+        if (!users) {
+            return res.status(404).json({ message: 'No users found' });
         }
         return res.status(200).json(users)
     }
-    catch(error){
-        res.status(500).json({message: error.message})
+    catch (error) {
+        res.status(500).json({ message: error.message })
     }
-} 
+}
+
+
+
 
 const signUp = async (req, res, next) => {
-    try{
-        const {name, email, password} = req.body;
-         //Validate input data
+    try {
+        const { name, email, password } = req.body;
+        //Validate input data
         if (!name || !email || !password) {
             return res.status(400).json({ message: 'All fields are required' });
         }
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message: 'Email already exists! Login instead'});
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: 'Email already exists! Login instead' });
         }
         // encrypting password
         const hashedPassword = bcrypt.hashSync(password);
         const user = new User({
-            name, 
-            email, 
+            name,
+            email,
             password: hashedPassword
         });
         await user.save();
-        return res.status(201).json({user})
+        return res.status(201).json({ user })
     }
-    catch(error){
-        res.status(500).json({message: error.message})
+    catch (error) {
+        res.status(500).json({ message: error.message })
     }
+}
 
 
 
 
-    // try{
-    //     await user.save();
-    // }
-    // catch(error){
-    //     res.status(500).json({message: error.message})
-    // }
+const login = async (req, res, next) => {
+    try {
+        const { email, password } = req.body;
+        //Validate input data
+        if (!email || !password) {
+            return res.status(400).json({ message: 'All fields are required' });
+        }
+        const existingUser = await User.findOne({ email });
+        if (!existingUser) {
+            return res.status(400).json({ message: 'Email does not exist! please sign up' });
+        }
 
-
+        const isPasswordCorrect = bcrypt.compareSync(password,existingUser.password);
+        if(!isPasswordCorrect){
+            return res.status(400).json({message: "incorrect password"})
+        }
+        return res.status(200).json({message: "login successful"})
+    }
+    catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 }
 
 module.exports = {
     getAllUsers,
-    signUp
+    signUp,
+    login
 }
--- routes/user.route.js
+++ routes/user.route.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { getAllUsers, signUp } = require('../controllers/users.controller')
+const { getAllUsers, signUp, login } = require('../controllers/users.controller')
 
 
 router.get('/',getAllUsers);
-router.post('/signup', signUp)
+router.post('/signup', signUp);
+router.get('/login', login)
 
 
 module.exports = router;
